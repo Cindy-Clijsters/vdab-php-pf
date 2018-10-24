0b26033d88bc81de89d6fbe5b0bc30e191413283
Cindy Clijsters <cindy.clijsters@vdabheverlee.intra>
2018-10-24 14:08:34 +0200

    Exercise 1.2

--- rekenmachine.php
+++ rekenmachine.php
@@ -8,7 +8,26 @@ class Rekenmachine {
     public function getSom($getal1, $getal2) {
         $resultaat = $getal1 + $getal2;
         return $resultaat;      
-    }    
+    }   
+    
+    /**
+     * Multiply 2 number
+     * 
+     * @param float $number1
+     * @param float $number2
+     * 
+     * @return float|string
+     */
+    public function multiply($number1, $number2)
+    {
+        if (is_numeric($number1) && is_numeric($number2)) {
+            $result = $number1 * $number2;
+        } else {
+            $result = "Beide parameters moeten een numeriek waarde bevatten.";
+        }
+        
+        return $result;
+    }
 }
 ?>
 
@@ -33,5 +52,7 @@ class Rekenmachine {
     <h1>
         <?php echo $reken->getSom(34, 55); ?>
     </h1>
+    
+    <h1><?php echo $reken->multiply(2, 4); ?></h1>
 </body>
 </html>
