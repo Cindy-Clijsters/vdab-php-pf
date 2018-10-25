1d239ae922103d0d2867b9a2b8b757ebb7ced18b
Cindy Clijsters <cindy.clijsters@gmail.com>
2018-10-25 14:15:08 +0200

    Exercise 2.7

--- analyse.php
+++ analyse.php
@@ -2,9 +2,27 @@
 
 class Oefening
 {
-    public function getAnalyse($getal1, $getal2)
+    /**
+     * Make an analyse of the numbers
+     * 
+     * @param int $number1
+     * @param int $number2
+     * 
+     * @return string
+     */
+    public function getAnalyse($number1, $number2)
     {
-        // ...  
+        if (is_numeric($number1) && is_numeric($number2)) {
+            if ($number1 > $number2) {
+                $result = "Het eerste getal is groter dan het tweede";
+            } else {
+                $result = "Het eerste getal is niet groter dan het tweede";
+            }
+        } else {
+            $result = "De beide getallen moeten een numerieke waarde bevatten";
+        }
+        
+        return $result;
     }
 }
 ?>
@@ -19,7 +37,19 @@ class Oefening
         <h1>
             <?php
             $oef = new Oefening();
-            print($oef->getAnalyse(7, 2));
+            echo $oef->getAnalyse(7, 2);
+            ?>
+        </h1>
+        <h1>
+            <?php
+            $oef = new Oefening();
+            echo $oef->getAnalyse(2, 7);
+            ?>
+        </h1>
+        <h1>
+            <?php
+            $oef = new Oefening();
+            echo $oef->getAnalyse(7, 7);
             ?>
         </h1>
     </body>
